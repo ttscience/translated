8d6cd0836c3f7d30c581d0e4302f1c54dab485fd
Laura Bąkała <erdaradun.gaztea@gmail.com>
2023-01-25 15:57:20 +0100

    add docs

--- R/trans_path.R
+++ R/trans_path.R
@@ -1,3 +1,21 @@
+#' Access translation path
+#'
+#' @description This function allows setting translation path and accessing the
+#' currently set one.
+#'
+#' @param path `character(1)`\cr
+#'  Path to set, should be a folder containing JSON files (possibly in
+#'  subdirectories).
+#'
+#' @return If `path` was not passed, currently set path, else nothing.
+#'
+#' @examples
+#' # Set path to example translations shipped with this package
+#' trans_path(system.file("examples", package = "translated"))
+#'
+#' # Check the current path
+#' trans_path()
+#'
 #' @export
 trans_path <- function(path) {
   if (missing(path)) {
--- R/trans_reload.R
+++ R/trans_reload.R
@@ -1,3 +1,12 @@
+#' Force translation reload
+#'
+#' @description This function forces the translations to be loaded from JSON
+#' files again.
+#'
+#' @examples
+#' trans_path(system.file("examples", package = "translated"))
+#' trans_reload()
+#'
 #' @importFrom utils assignInMyNamespace
 #' @export
 trans_reload <- function() {
