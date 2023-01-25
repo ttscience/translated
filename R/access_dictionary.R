5544683f7851141d24de805ba5b72c6c982f6552
Laura Bąkała <erdaradun.gaztea@gmail.com>
2023-01-25 15:54:45 +0100

    move read_by_key()

--- R/access_dictionary.R
+++ R/access_dictionary.R
@@ -24,8 +24,3 @@ get_dictionary <- function() {
   }
   country_map[[country]]
 }
-
-read_by_key <- function(dict, key) {
-  key <- strsplit(key, ".", fixed = TRUE)[[1]]
-  Reduce(`[[`, key, init = dict[["translation"]])
-}
