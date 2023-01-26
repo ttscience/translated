73f9325abf6c62c1d149fd7940bf01d5c1586b07
Laura Bąkała <erdaradun.gaztea@gmail.com>
2023-01-27 00:51:53 +0100

    implement two tests for the start

---
+++ tests/testthat/setup-dict.R
@@ -0,0 +1,22 @@
+dict_1 <- list(
+  config = list(
+    locale = list(
+      language = "en",
+      country = "CA",
+      encoding = NULL
+    ),
+    plural = list(
+      c("n == 1", "2"),
+      c("TRUE", "3")
+    )
+  ),
+  translation = list(
+    language = "Canadian English",
+    nouns = list(
+      animals = list(
+        cat = "catty",
+        dog = "doggie"
+      )
+    )
+  )
+)
