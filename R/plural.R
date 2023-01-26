64565354a7816de3336e7c85a4faa188b55baca3
Laura Bąkała <erdaradun.gaztea@gmail.com>
2023-01-26 16:33:02 +0100

    implement custom plural engine
    close #7

--- R/plural.R
+++ R/plural.R
@@ -1,5 +1,9 @@
-#' @importFrom dplyr case_when
-choose_plural_case <- function(dict, .n) {
-  plural <- dict[["config"]][["plural"]]
-  eval(str2lang(plural))
+choose_plural_case <- function(dict, n) {
+  definition <- dict[["config"]][["plural"]]
+  cases <- strsplit(definition, ",", fixed = TRUE)[[1]]
+  conditions <- strsplit(cases, "~", fixed = TRUE)
+  ret <- Find(function(cond) {
+    eval(str2lang(cond[1]))
+  }, conditions)
+  eval(str2lang(ret[2]))
 }
