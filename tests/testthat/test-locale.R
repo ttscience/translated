514f16c52fe3ad4c4bc7a90683a582881171a203
Laura Bąkała <erdaradun.gaztea@gmail.com>
2023-01-27 12:35:20 +0100

    test locale validator

--- tests/testthat/test-locale.R
+++ tests/testthat/test-locale.R
@@ -1,6 +1,34 @@
 # interpret_locale ------------------------------------------------------------
 
 # is_valid_locale -------------------------------------------------------------
+test_that("language only is a valid locale", {
+  expect_true(is_valid_locale("eu"))
+})
+
+test_that("country code may be added after an underscore", {
+  expect_true(is_valid_locale("eu_ES"))
+})
+
+test_that("encoding can be included after a dot", {
+  expect_true(is_valid_locale("eu_ES.utf8"))
+})
+
+test_that("language must be a two-letter code", {
+  expect_false(is_valid_locale("eus"))
+  expect_false(is_valid_locale("eus_ES"))
+})
+
+test_that("country code must be a two-letter code", {
+  expect_false(is_valid_locale("eu_France"))
+})
+
+test_that("separators cannot be altered", {
+  expect_false(is_valid_locale("eu-ES"))
+})
+
+test_that("encoding cannot be given without a country code", {
+  expect_false(is_valid_locale("eu.utf8"))
+})
 
 # null_if_empty ---------------------------------------------------------------
 test_that("null_if_empty() returns original string if not empty", {
