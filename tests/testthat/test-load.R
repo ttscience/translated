4e34d4caff3e36f57c6312b72c204dfd58697564
Laura <erdaradun.gaztea+github@gmail.com>
2023-01-27 20:24:58 +0100

    add one more check

--- tests/testthat/test-load.R
+++ tests/testthat/test-load.R
@@ -13,6 +13,15 @@ test_that("each locale is a `trns_locale` object after standardization", {
   }
 })
 
+test_that("translation data is left unchanged", {
+  for (i in seq_along(std_json_data)) {
+    expect_equal(
+      std_json_data[[i]][["translation"]],
+      json_data[[i]][["translation"]]
+    )
+  }
+})
+
 test_that("NULL country is replaced with '_unknown'", {
   json_pl <- Find(function(json) {
     json[["config"]][["locale"]][["language"]] == "pl"
